fd7e74f5cb297129bbfce04f8372fb6900af97c6
Akhilesh Sharma <akhilesharma47@gmail.com>
2021-07-10 19:13:58 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,19 +1,28 @@
-from tf.keras.models import Model
-from tf.keras.layers import *
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import *
+import tensorflow_addons as tfa
 
 class ResUnet(object):
     
-    def __init__(self, num_classes, input_shape):
+    def __init__(self, num_classes, input_shape, layer_norm='batch'):
         self.num_classes = num_classes
         self.height, self.width, self.channels = input_shape
+        self.layer_norm = layer_norm
+
+    def normalization(self, x, layer_norm):
+        if layer_norm == 'batch':
+            x = BatchNormalization()(x)
+        else:
+            x = tfa.layers.InstanceNormalization()(x)
+        return x
 
     def residual_block(self, input, filters, dilation_rates):
         out = [input]
         for rate in dilation_rates:
-            x = BatchNormalization()(input)
+            x = self.normalization(input, self.layer_norm)
             x = Activation('relu')(x)
             x = Conv2D(filters, 3, dilation_rate = rate, padding='same')(x)
-            x = BatchNormalization()(x)
+            x = self.normalization(x, self.layer_norm)
             x = Activation('relu')(x)
             x = Conv2D(filters, 3, dilation_rate = rate, padding='same')(x)
             out.append(x)
@@ -26,26 +35,26 @@ class ResUnet(object):
             x = MaxPooling2D(pool_size = i, strides = i)(input)
             x = UpSampling2D(size = i)(x)
             x = Conv2D((filters//4), 1, padding='same')(x)
-            x = BatchNormalization()(x)
+            x = self.normalization(x, self.layer_norm)
             out.append(x)
         if height==448 and filters==1024:
             out[3] = ZeroPadding2D((1, 1))(out[3])
         out = Concatenate(axis=-1)([out[0],out[1],out[2],out[3]])
         out = Conv2D(filters, 1, padding = 'same')(out)
-        out = BatchNormalization()(out)
+        out = self.normalization(out, self.layer_norm)
         return out
 
     def combine(self, x, y, filters):
         x = UpSampling2D(size = 2, interpolation='bilinear')(x)
         x = Concatenate(axis=-1)([x,y])
         x = Conv2D(filters, 1, padding = 'same')(x)
-        x = BatchNormalization()(x)
+        x = self.normalization(x, self.layer_norm)
         return x
 
     def build_model(self):
         input = Input(shape=(self.height,self.width, self.channels))
         x = Conv2D(32, 1, padding= 'same')(input)               #Layer-1
-        x = BatchNormalization()(x)
+        x = self.normalization(x, self.layer_norm)
         x = Activation('relu')(x)
         a = x
         x = self.residual_block(x, 32, [1,3,15,31])             #Layer-2
@@ -93,34 +102,32 @@ class ResUnet(object):
         
         dist = ZeroPadding2D(padding=1)(x1)
         dist = Conv2D(32, 3)(dist)
-        dist = BatchNormalization()(dist)
+        dist = self.normalization(dist, self.layer_norm)
         dist = Activation('relu')(dist)
         dist = ZeroPadding2D(padding=1)(dist)
         dist = Conv2D(32, 3)(dist)
-        dist = BatchNormalization()(dist)
+        dist = self.normalization(dist, self.layer_norm)
         dist = Activation('relu')(dist)
         dist = Conv2D(self.num_classes, 1, activation='softmax', name = 'distance')(dist)
 
         bound = Concatenate(axis=-1)([x, dist])
         bound = ZeroPadding2D(padding=1)(bound)
         bound = Conv2D(32, 3)(bound)
-        bound = BatchNormalization()(bound)
+        bound = self.normalization(bound, self.layer_norm)
         bound = Activation('relu')(bound)
         bound = Conv2D(self.num_classes, 1, activation='sigmoid', name = 'boundary')(bound)
-
-        color = Conv2D(3, 1, activation = 'sigmoid', name = 'color')(x1)
 
         seg = Concatenate(axis=-1)([x,bound,dist])
         seg = ZeroPadding2D(padding=1)(seg)
         seg = Conv2D(32, 3)(seg)
-        seg = BatchNormalization()(seg)
+        seg = self.normalization(seg, self.layer_norm)
         seg = Activation('relu')(seg)
         seg = ZeroPadding2D(padding=1)(seg)
         seg = Conv2D(32, 3)(seg)
-        seg = BatchNormalization()(seg)
+        seg = self.normalization(seg, self.layer_norm)
         seg = Activation('relu')(seg)
         seg = Conv2D(self.num_classes, 1, activation='softmax', name = 'segmentation')(seg)
 
-        model = Model(inputs = input, outputs={'seg': seg, 'bound': bound, 'dist': dist, 'color' : color})
+        model = Model(inputs = input, outputs={'seg': seg, 'bound': bound, 'dist': dist})
         
         return model
